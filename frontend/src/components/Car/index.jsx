34e0fd806ff7788567e93ea392855e866ee4241d
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-03-31 11:47:23 -0300

    update index

--- frontend/src/components/Car/index.jsx
+++ frontend/src/components/Car/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Field, Formik, Form } from 'formik'
-import api from '../../servicos/api'
+import api from '../../services/api'
 import './styles.scss'
 import Swal from 'sweetalert2'
 
@@ -46,7 +46,7 @@ const Car = () => {
 
 
     try {
-      const response = await api.post(`/carro`, {
+      const response = await api.post(`/car`, {
         modelo: modeloInfo,
         ano: +anoInfo,
         marca: marcaInfo,
