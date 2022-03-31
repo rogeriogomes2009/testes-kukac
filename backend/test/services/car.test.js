8a790d88b37b7aa61b6e21feccebec871651c111
Rogério Gomes <95705053+rogeriogomes2009@users.noreply.github.com>
2022-03-31 10:46:33 -0300

    Update car.test.js

--- backend/test/services/car.test.js
+++ backend/test/services/car.test.js
@@ -1,4 +1,13 @@
-exports.DATABASE = {
-  cars: [],
-  motocycles: [],
-};
+const { Car } = require("../../src/app/services/CarServices");
+
+describe("Testando serviços de carros", () => {
+  it("Precisa devolver carro = {modelo: 'Fiesta', ano: 2019, portas: 4, marca: 'Ford' }", () => {
+    const result = new Car("Fiesta", 2019, 4, "Ford");
+    expect(result).toEqual({
+      modelo: "Uno",
+      ano: 2019,
+      portas: 4,
+      marca: "Ford",
+    });
+  });
+});
