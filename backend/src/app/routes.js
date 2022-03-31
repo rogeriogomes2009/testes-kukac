dfaeb8805fae997711c4fed5fa7991401b005b52
rogeriogomes2009 <rogerio.gomes2009@gmail.com>
2022-03-31 12:06:28 -0300

    update back

--- backend/src/app/routes.js
+++ backend/src/app/routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 
 const PalindController = require("./controllers/PalindController.js");
-const PurchController = require("./controllers/PurshController.js");
+const PurchController = require("./controllers/PurchController.js");
 const MotoController = require("./controllers/MotoController.js");
 const CarController = require("./controllers/CarController.js");
 const CepController = require("./controllers/CepController.js");
@@ -9,11 +9,11 @@ const CepController = require("./controllers/CepController.js");
 const routes = Router();
 
 routes.get("/palind", PalindController.show);
-routes.get("/compras", PurchController.show);
+routes.get("/purchase", PurchController.show);
 routes.post("/moto", MotoController.store);
 routes.get("/moto", MotoController.show);
-routes.post("/carro", CarController.store);
-routes.get("/carro", CarController.show);
+routes.post("/car", CarController.store);
+routes.get("/car", CarController.show);
 routes.post("/ceps", CepController.show);
 
 module.exports = routes;
