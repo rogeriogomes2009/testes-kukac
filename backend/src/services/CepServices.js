7242e4a734e0238ad492b091c69af5b53ebe7e3f
Rogério Gomes <95705053+rogeriogomes2009@users.noreply.github.com>
2022-03-31 10:39:22 -0300

    add CepServices

---
+++ backend/src/services/CepServices.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const baseUrl = `https://viacep.com.br/ws`;
+
+const CEPs = async (ceps = []) => {
+  const arrAddresses = [];
+
+  const consumingAPI = await Promise.all(
+    ceps.map((cep) => axios.get(`${baseUrl}/${cep}/json/`))
+  );
+  consumingAPI.map((address) => arrAddresses.push(address.data));
+
+  return arrAddresses;
+};
+
+module.exports = CEPs;
