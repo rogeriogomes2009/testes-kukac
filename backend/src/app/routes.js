7cf904056084313c3c641b4c41ad663c39397f4e
Rogério Gomes <95705053+rogeriogomes2009@users.noreply.github.com>
2022-03-31 10:31:02 -0300

    Create routes.js

---
+++ backend/src/app/routes.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const PalindController = require("./controllers/PalindController.js");
+const PurchController = require("./controllers/PurshController.js");
+const MotoController = require("./controllers/MotoController.js");
+const CarController = require("./controllers/CarController.js");
+const CepController = require("./controllers/CepController.js");
+
+const routes = Router();
+
+routes.get("/palind", PalindController.show);
+routes.get("/compras", PurchController.show);
+routes.post("/moto", MotoController.store);
+routes.get("/moto", MotoController.show);
+routes.post("/carro", CarController.store);
+routes.get("/carro", CarController.show);
+routes.post("/ceps", CepController.show);
+
+module.exports = routes;
